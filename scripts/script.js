1fbb5124f5a14ef86ec73b4348a4017f2c62edf7
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-12-04 11:06:29 +0100

    update of all updates
    Navigation, parallax, etc

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+//NAVIGATION SCRIPT START
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".menu");
+const btn = document.querySelector(".btn");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    btn.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n =>
+n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    btn.classList.toggle("active");
+}))
+//NAVIGATION SCRIPT END
+
+
+//PARALLAX SCROLLING EFFECT START
+const parallax = document.getElementsByClassName('hero');
+window.addEventListener('scroll', function(){
+  let offset = window.pageYOffset;
+  parallax.style.backgroundPositionY = offset * 0.7 + 'px'
+})
+//PARALLAX SCROLLING EFFECT END
